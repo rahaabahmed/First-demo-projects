54d77e4e48740e489d306c03e45f3c1be9d2c485
rahaabahmed <rahaabahmed9988@gmail.com>
2025-12-30 14:47:02 +0500

    Add files via upload

---
+++ Python Practice/1st-assignment.py
@@ -0,0 +1,22 @@
+# ---------------------Program to check even or odd number--------------------------
+
+n = int(input("Enter a number :"))
+
+if( n % 2 == 0):
+    print("Even")
+else:
+    print("Odd")    
+
+
+
+#  --------------------Program to check positive , negative or zero---------------------
+
+
+N = float(input("Enter a number :"))
+
+if( N > 0 ):
+    print("Postive")
+elif( N < 0 ):
+    print("Negative")
+else:
+    print("Zero")        
